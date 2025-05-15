572cab074d8470c8a5db7b40b405dc62e99b397b
Stefan He <hebiaobuaa@gmail.com>
2025-05-14 23:50:41 -0700

    Update simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -15,12 +15,19 @@ normal_tensor = torch.full((1_000_000_000,), 100, dtype=torch.uint8, device='cud
 with memory_saver.region():
     pauseable_tensor = torch.full((1_000_000_000,), 100, dtype=torch.uint8, device='cuda')
 
-print(f'{normal_tensor=} {pauseable_tensor=}')
+print(f'After Created: {normal_tensor=}\n{pauseable_tensor=}')
+print(f"Allocated: {torch.cuda.memory_allocated() / 1024**2:.2f} MB")
+print(f"Reserved: {torch.cuda.memory_reserved() / 1024**2:.2f} MB")
+
 
 print('sleep...')
 time.sleep(3)
 
 memory_saver.pause()
+
+print(f'After Paused: {normal_tensor=}\n{pauseable_tensor=}')
+print(f"Allocated: {torch.cuda.memory_allocated() / 1024**2:.2f} MB")
+print(f"Reserved: {torch.cuda.memory_reserved() / 1024**2:.2f} MB")
 
 print('sleep...')
 time.sleep(3)
@@ -30,4 +37,7 @@ memory_saver.resume()
 print('sleep...')
 time.sleep(3)
 
-print(f'{normal_tensor=} {pauseable_tensor=}')
+print(f'After Resumed: {normal_tensor=}\n{pauseable_tensor=}')
+print(f"Allocated: {torch.cuda.memory_allocated() / 1024**2:.2f} MB")
+print(f"Reserved: {torch.cuda.memory_reserved() / 1024**2:.2f} MB")
+
