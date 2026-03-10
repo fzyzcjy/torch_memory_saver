c8097400cb17f7a579d618ca7c4b8a3f49f3c35e
yueming-yuan <yym022502@gmail.com>
2026-03-09 23:08:29 -0700

    update

--- torch_memory_saver/entrypoint.py
+++ torch_memory_saver/entrypoint.py
@@ -126,9 +126,8 @@ class _TorchMemorySaverImpl:
 
     @contextmanager
     def _with_region_config(self, tag: str, enable_cpu_backup: bool):
-        cdll = self._binary_wrapper.cdll
-        original_tag = cdll.tms_get_interesting_region()  # save whether we were already in a region
-        original_enable_cpu_backup = cdll.tms_get_enable_cpu_backup()
+        orig_interesting_region = self._binary_wrapper.cdll.tms_get_interesting_region()
+        orig_enable_cpu_backup = self._binary_wrapper.cdll.tms_get_enable_cpu_backup()
 
         self._binary_wrapper.set_config(tag=tag, interesting_region=True, enable_cpu_backup=enable_cpu_backup)
         try:
@@ -136,8 +135,8 @@ class _TorchMemorySaverImpl:
         finally:
             self._binary_wrapper.set_config(
                 tag=_TAG_DEFAULT,
-                interesting_region=original_tag,
-                enable_cpu_backup=original_enable_cpu_backup,
+                interesting_region=orig_interesting_region,
+                enable_cpu_backup=orig_enable_cpu_backup,
             )
 
     @contextmanager
