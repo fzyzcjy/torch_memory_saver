cd7b4b65c0b570f0d2c43bba775d19847280f93f
Schwinn Saereesitthipitak <schwinns@nvidia.com>
2026-07-01 03:09:25 -0700

    feat: add pluggable hook modes
    Signed-off-by: Schwinn Saereesitthipitak <schwinns@nvidia.com>

--- torch_memory_saver/__init__.py
+++ torch_memory_saver/__init__.py
@@ -1,5 +1,17 @@
-from .entrypoint import TorchMemorySaver
-from .hooks.mode_preload import configure_subprocess
+from contextlib import contextmanager
+
+from .entrypoint import TorchMemorySaver, register_hook_mode
+from .hooks import mode_preload as _mode_preload
 
 # Global singleton
 torch_memory_saver = TorchMemorySaver()
+
+
+@contextmanager
+def configure_subprocess():
+    if not torch_memory_saver._uses_preload():
+        yield
+        return
+
+    with _mode_preload.configure_subprocess():
+        yield
